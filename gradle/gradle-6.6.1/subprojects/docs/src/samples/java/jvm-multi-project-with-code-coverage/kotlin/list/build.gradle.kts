7b795c3df6252cca0c777f1a231a2e03c8d6a0e7
Dylan Bray <dfbray@utexas.edu>
2020-11-01 21:02:04 -0600

    Gradle guava works for real now

--- gradle/gradle-6.6.1/subprojects/docs/src/samples/java/jvm-multi-project-with-code-coverage/kotlin/list/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    `java-library`
-    `jacoco`
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
